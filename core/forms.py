572d73c49006ee9bc70d3c1b2f54b528791b478c
Jaicker Rafael Lozano Flores <jlozano.devcode@gmail.com>
2026-04-18 23:03:01 -0400

    feat: aplicar estilos modernos al formulario de inicio de sesión

--- core/forms.py
+++ core/forms.py
@@ -48,12 +48,15 @@ class LoginForm(AuthenticationForm):
         super().__init__(*args, **kwargs)
         self.fields['username'].label = "Nombre de usuario"
         self.fields['password'].label = "Contraseña"
+        
+        # Agregamos las clases de Tailwind y Flowbite directamente a los inputs
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-brand-blue focus:border-brand-blue block w-full p-2.5'
+            })
 
 
 class ContactForm(forms.Form):
     name = forms.CharField(label='Nombre', max_length=100)
     email = forms.EmailField(label='Correo electrónico')
     message = forms.CharField(label='Mensaje', widget=forms.Textarea)
-
-
-
