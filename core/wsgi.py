62c5ec8989d890663483e5a76b942ed97d74d880
Jaicker Rafael Lozano Flores <jlozano.devcode@gmail.com>
2026-04-23 10:23:52 -0400

    Configurando WhiteNoise y wsgi.py para cargar archivos media

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
+# Obtiene la aplicación WSGI estándar de Django
 application = get_wsgi_application()
+
+# Envuelve la aplicación con WhiteNoise.
+# Primero, le decimos que sirva los archivos estáticos desde STATIC_ROOT (los que recoge `collectstatic`).
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
+
+# Luego, añadimos la regla para servir los archivos de medios desde MEDIA_ROOT en la URL MEDIA_URL.
+application.add_files(settings.MEDIA_ROOT, prefix=settings.MEDIA_URL)
